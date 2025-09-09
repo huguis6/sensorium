83dd4f1f3c67b4546b26668d81932842d96165b6
huguis6 <133821416+huguis6@users.noreply.github.com>
2025-09-09 16:49:20 -0300

    subo la pagina

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){       
+   var scroll_start = 0;
+   var startchange = $('#startchange');
+   var offset = startchange.offset();
+    if (startchange.length){
+   $(document).scroll(function() { 
+      scroll_start = $(this).scrollTop();
+      if(scroll_start > offset.top) {
+          $(".navbar-default").css('background-color', '#f0f0f0 !important');
+       } else {
+          $('.navbar-default').css('background-color', 'transparent ');
+       }
+   });
+    }
+});
+
+tailwind.config = {
+   theme: {
+     extend: {
+      backgroundImage: {
+        'hero-img': "url('https://picsum.photos/1920/1920/?random')",
+        'body-img': "url('https://picsum.photos/1600/1600/?random')",
+      }
+    }
+   }
+ }
